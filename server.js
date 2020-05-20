ba5b43a5758928758d62e17a6cc25e727138cc91
Sudarshan Agrawal <sudarshan.shubham@gmail.com>
2020-05-20 17:11:27 -0400

    changed mongodb uri

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 // setup database
 
-const uri = process.env.ATLAS_URI;
+const uri = process.env.MONGODB_URI;
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useCreateIndex: true,
